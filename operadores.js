d857243ad01e37e105584121f8cbbc8ae473732b
mariosepulveda3 <mariosepul3@gmail.com>
2022-10-03 22:14:07 +0200

    ADD, operadores

--- operadores.js
+++ operadores.js
@@ -1,23 +1,35 @@
 /*1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.*/
 
-
+let mult = 10 * 5;
+console.log(mult);
 
 
 /*1.2 Divide 10 por 2 y muestra el resultado en un alert.*/
 
-
-
+let div = 10 / 2;
+console.log(div);
 
 /*1.3 Muestra mediante un alert el resto de dividir 15 por 9.*/
 
-
-
+let rest = 15 - 9;
+console.log(rest);
 
 /*1.4 Usa el correcto operador de asignación que resultará en x = 15, 
 teniendo dos variables y = 10 y z = 5.*/
 
-
-
+let y = 10;
+let z = 5;
+let x = y + z;
+console.log(x);
 
 /*1.5 Usa el correcto operador de asignación que resultará en x = 50,
 teniendo dos variables y = 10 y z = 5.*/
+
+let y = 10;
+let z = 5;
+let x = y * z;
+console.log(x);
+
+
+/* AL VER LA SOLUCIÓN, HE COMPROBADO QUE PARA RESTAR SE UTILIZA < % >.
+   Y AL BUSCAR LA IMPRESIÓN EN CONSOLE CON NODE, ME SALE ERROR, SIN SABER POR QUÉ.*/
