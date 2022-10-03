60ca98e3d90380e2936acfc5280196936b7401aa
mariosepulveda3 <mariosepul3@gmail.com>
2022-10-03 21:38:48 +0200

    ADD, exercise variables

--- variables.js
+++ variables.js
@@ -1,13 +1,20 @@
 //1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
 
-
+let myFavoriteHero = 'Hulk';
 
 //1.2 Crea una variable llamada x, asigna el valor 50 a ella.
 
-
+let x = 50;
 
 //1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
 
-
+let h = 5;
+let y = 10;
 
 //1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+
+let z = 'h' + 'y';
+
+
+/* AL VER LA SOLUSIÓN, HE COMPROBADO QUE EN EL EJERCICIO 1.4, 
+   HE PUESTO LOS VALORES < 'h' + 'y' > EN VEZ DE < h + y >.*/
