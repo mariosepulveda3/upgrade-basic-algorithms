d225028f64963b5cdc30220c243a01e195bc99fe
mariosepulveda3 <mariosepul3@gmail.com>
2022-10-03 22:54:34 +0200

    ADD, variables avanzadas

--- variables-avanzadas.js
+++ variables-avanzadas.js
@@ -1,7 +1,7 @@
 /*1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.
 const character = {name: 'Jack Sparrow', age: 10};*/
 
-
+const character = {name: 'Jack sparrow', age: 10};
 
 /*1.2 Declara 3 variables con los nombres y valores siguientes 
 	firstName = 'Jon'; 
@@ -10,14 +10,19 @@ const character = {name: 'Jack Sparrow', age: 10};*/
 Muestralos por consola de esta forma: 
 	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'*/
 
-
+let firstName = 'Jon';
+let lastName = 'Snow';
+let age = 24;
+console.log ('Soy ' + firstName + lastName + ', tengo ' + age + ' y me gustan los lobos.');
 
 /*1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
 ambos juguetes.
 const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};*/
 
-
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+console.log (price 'toy1' + price 'toy2');
 
 /*1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
 y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
@@ -25,3 +30,16 @@ basePrice más el valor de la variable globalBasePrice.
 let globalBasePrice = 10000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};*/
+
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+let globalBasePrice = 25000;
+
+car1 finalPrice = globalBasePrice + car1 basePrice;
+car2 finalPrice = globalBasePrice + car2 basePrice;
+
+/* AL VER LA SOLUCIÓN:
+   1.2 : ME HAN FALTADO ESPACIOS PARA SEPARAR LAS VARIABLES.
+   
