600407630ce8e948d2ee9811b6f1b877d8127961
mariosepulveda3 <mariosepul3@gmail.com>
2022-10-04 09:41:00 +0200

    CORRECTED, variables avanzadas

--- variables-avanzadas.js
+++ variables-avanzadas.js
@@ -13,7 +13,7 @@ Muestralos por consola de esta forma:
 let firstName = 'Jon';
 let lastName = 'Snow';
 let age = 24;
-console.log ('Soy ' + firstName + lastName + ', tengo ' + age + ' y me gustan los lobos.');
+console.log ('Soy ' + firstName + " " + lastName + ', tengo ' + age + ' y me gustan los lobos.');
 
 /*1.3 Dado el siguiente javascript, imprime con un console.log la suma del precio de
 ambos juguetes.
@@ -22,7 +22,7 @@ const toy2 = {name: 'Rallo mcKing', price: 29};*/
 
 const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};
-console.log (price 'toy1' + price 'toy2');
+console.log (toy1.price + toy2.price);
 
 /*1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000 
 y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad 
@@ -35,10 +35,11 @@ let globalBasePrice = 10000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 
-let globalBasePrice = 25000;
+globalBasePrice = 25000;
 
-car1 finalPrice = globalBasePrice + car1 basePrice;
-car2 finalPrice = globalBasePrice + car2 basePrice;
+car1.finalPrice = globalBasePrice + car1.basePrice;
+car2.finalPrice = globalBasePrice + car2.basePrice;
+console.log (car1, car2);
 
 /* AL VER LA SOLUCIÓN:
    1.2 : ME HAN FALTADO ESPACIOS PARA SEPARAR LAS VARIABLES.
