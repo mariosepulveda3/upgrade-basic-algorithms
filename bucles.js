a1fdf2f502cb0103dbdd64ae2a425cbf2eb47349
mariosepulveda3 <mariosepul3@gmail.com>
2022-10-05 14:53:36 +0200

    EXERCISE, bucles

--- bucles.js
+++ bucles.js
@@ -1,20 +1,23 @@
 /*1.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.*/
 
 for (let m = 0; m < 10; m++) {
-console.log(m);
+    console.log(m);
 }
 
 /*1.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
 cuando el resto del numero dividido entre 2 sea 0.*/
 
-for ( var x = 0; x <= 9; x++) {
-    if (x % 2 ==0) {
-        console.log(x);
-    }
-}
+for (let x = 0; x < 10; m /= 2) {
+    console.log(m);     
+ }
+
 
 /*1.3 Crea un bucle para conseguir dormir contando ovejas. 
 Este bucle empieza en 0 y termina en 10. 
 Muestra por consola un mensaje diciendo 'Intentando dormir' en cada vuelta del bucle 
 y cambia el mensaje en la última vuelta a 'Dormido!'.*/
+
+for (let dormir = 0; dormir < 11; dormir++) {
+    console.log ('Intentando dormir ' + dormir + ' . Dormido!');
+}
 
